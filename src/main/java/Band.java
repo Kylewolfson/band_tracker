cc717a21e684c67ab6c2d71cf219c58a6de9bf0c
Kyle Wolfson <wolfsonk@gmail.com>
2016-05-13 08:23:16 -0700

    added basic object functions for band and venue objects. Functions are: constructor, getName, getId, all, equals, save and find.

---
+++ src/main/java/Band.java
@@ -0,0 +1,57 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
+
+public class Band {
+  private int id;
+  private String name;
+  private int venues_id;
+
+  public Band(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Band> all() {
+    String sql = "SELECT * FROM bands";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Band.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherBand){
+    if (!(otherBand instanceof Band)) {
+      return false;
+    } else {
+      Band newBand = (Band) otherBand;
+      return this.getId() == newBand.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO bands (name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static Band find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM bands WHERE id=:id";
+      Band band = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Band.class);
+      return band;
+    }
+  }
---
+++ src/main/java/Venue.java
@@ -0,0 +1,57 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
+
+public class Venue {
+  private int id;
+  private String name;
+  private int bands_id;
+
+  public Venue(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Venue> all() {
+    String sql = "SELECT * FROM venues";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Venue.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherVenue){
+    if (!(otherVenue instanceof Venue)) {
+      return false;
+    } else {
+      Venue newVenue = (Venue) otherVenue;
+      return this.getId() == newVenue.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO venues (name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static Recipe find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM venues WHERE id=:id";
+      Venue venue = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Venue.class);
+      return venue;
+    }
+  }
